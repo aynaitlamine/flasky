3a11b559324d44f45abaa59afa22bc115edbdf7c
aynaitlamine <aynaitlamine.dev@gmail.com>
2022-08-15 09:22:28 +0100

    added  mail service

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, request, make_response, render_template, session, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_mail import Mail, Message
 from forms import *
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+mail = Mail(app)
 
 ### Models ###
 
@@ -23,6 +25,16 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+
+### Custom functions ####
+
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
+                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    mail.send(msg)
 
 
 @app.route("/")
@@ -43,13 +55,15 @@ def add():
         if user is None:
             user = User(username=form.name.data)
             db.session.add(user)
-            db.session.commit()
             session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_email(app.config['FLASKY_ADMIN'],
+                           'New User', 'mail/new_user', user=user)
         else:
             session['known'] = True
         session['name'] = form.name.data
         form.name.data = ''
-        # return redirect(url_for('index'))
+        return redirect(url_for('index'))
     return render_template('pages/add.html',
                            form=form, name=session.get('name'),
                            known=session.get('known', False))
--- config.py
+++ config.py
@@ -2,3 +2,13 @@ import os
 SECRET_KEY = os.urandom(32)  # For initialize flask-wtforms
 SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost:5432/flasky"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+MAIL_SERVER = 'smtp.mailtrap.io'
+MAIL_PORT = 2525
+MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+MAIL_USE_TLS = True
+MAIL_USE_SSL = False
+FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+
+FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
